a4cd4e51c88482b75d8c99ea1113bd97392c4640
Ben <ben.white101@hotmail.co.uk>
2020-02-06 12:37:09 +0000

    Poppins Font link

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './Todo.css'
 
+<link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,900&display=swap" rel="stylesheet"/> 
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
