961d3a8e312efc6a75f8c3206398940ba77b25a7
Ben <ben.white101@hotmail.co.uk>
2020-02-06 12:26:01 +0000

    Fix Input

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
   return (
     <>
     <TodoList todos={todos} toggleTodo={toggleTodo} />  
-    <input id="enterTodo" ref={todoNameRef} onKeyPress={this.keyPressed} type="text" />
+    <input id="enterTodo" ref={todoNameRef} /*onKeyPress={this.keyPressed}*/ type="text" />
     <button class="btn add" onClick={handleAddTodo}>Add Todo</button>
     <button class="btn clear" onClick={handleClearTodos}>Clear Completed</button>
     <div class="amount"><span>{todos.filter(todo => !todo.complete).length}</span> left todo bro</div>
