736ae0130d142df760da5189af795e54021010c1
BenWhite101 <ben.white101@hotmail.co.uk>
2026-02-02 14:24:26 +0000

    edit btn

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Todo from './Todo'
 
-export default function TodoList({ todos, toggleTodo }) { //pass down prop
+export default function TodoList({ todos, toggleTodo, updateTodoName, onEditSubmit }) { //pass down prop
     return (
         todos.map(todo => {
-            return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />
+            return (
+                <Todo
+                    key={todo.id}
+                    toggleTodo={toggleTodo}
+                    updateTodoName={updateTodoName}
+                    onEditSubmit={onEditSubmit}
+                    todo={todo}
+                />
+            )
         })
     )
 }
